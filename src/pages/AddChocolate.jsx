de21bbc6a20c669b3e16e3218b6eddea0e3b5064
hossainshakil242 <hossainshakil242@gmail.com>
2023-09-03 17:02:39 +0600

    get data from form

--- src/pages/AddChocolate.jsx
+++ src/pages/AddChocolate.jsx
@@ -4,7 +4,22 @@ const AddChocolate = () => {
 
     const handleAddNewChocololate = e => {
         e.preventDefault();
-        console.log('shakil')
+        const form = e.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const country = form.country.value;
+        const quantity = form.quantity.value;
+        const date = form.date.value;
+        const brand = form.brand.value;
+        const color = form.color.value;
+        const file = form.file.value;
+        const photoUrl = form.photoUrl.value;
+        const chocolateDiscription = form.chocolateDiscription.value;
+
+
+        const chocolate = { name, price, country, color, quantity, brand, date, file,photoUrl,chocolateDiscription }
+
+        console.log(chocolate);
     }
     return (
         <div className='bg-[#7B3F00] m-5 md:m-12 rounded-xl'>
@@ -21,7 +36,7 @@ const AddChocolate = () => {
 
                     {/* Chocolate country and quantity */}
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-3 justify-items-center'>
-                        <select className="select select-acent w-full">
+                        <select name='country' className="select select-acent w-full">
                             <option disabled selected>Impot of Country</option>
                             <option value='Bangladesh'>Bangladesh</option>
                             <option value='India'>India</option>
@@ -33,8 +48,8 @@ const AddChocolate = () => {
 
                     {/* Chocolate date url and brad name */}
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-3 justify-items-center'>
-                        <input type="date" name='quantity' placeholder="Chocolate Quantity" className="input input-bordered input-acent w-full" />
-                        <select className="select select-acent w-full">
+                        <input type="date" name='date' placeholder="Buy Date" className="input input-bordered input-acent w-full" />
+                        <select name='brand' className="select select-acent w-full">
                             <option disabled selected>Name Of Brand</option>
                             <option value='Toblerone'>Toblerone</option>
                             <option value='Cadbury'>Cadbury</option>
@@ -45,17 +60,24 @@ const AddChocolate = () => {
 
                     {/* Chocolate photo url and Color code */}
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-3 justify-items-center'>
-                        <select className="select select-acent w-full">
-                            <option disabled selected>Impot of Country</option>
-                            <option value='Bangladesh'>Bangladesh</option>
-                            <option value='India'>India</option>
-                            <option value='America'>America</option>
-                            <option value='Nepal'>Nepal</option>
+                        <select name='color' className="select select-acent w-full">
+                            <option disabled selected>Select Color</option>
+                            <option value='Black'>Black</option>
+                            <option value='Dark_Green'>Dark_Green</option>
+                            <option value='Light'>Light</option>
+                            <option value='Umber'>Umber</option>
                         </select>
-                        <input type="text" name='quantity' placeholder="Chocolate Quantity" className="input input-bordered input-acent w-full" />
+                        <input type="file" name='file' placeholder="Chocolate Quantity" className="input input-bordered input-acent w-full" />
                     </div>
 
+                    {/* Chocolate chocolate photo url */}
+                    <div className='grid grid-cols-1 justify-items-center'>
+                    <input type="url" name='photoUrl' placeholder="Chocolate Photo URL" className="input input-bordered input-acent w-full" />
+                    </div>
                     {/* Chocolate discriptions */}
+                    <div className='grid grid-cols-1 justify-items-center'>
+                        <textarea name='chocolateDiscription' className="textarea textarea-bordered w-full" placeholder="Write Chocolate Discription Here....."></textarea>
+                    </div>
 
 
                     {/* submit button */}
